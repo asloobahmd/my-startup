c80df443706ae66ac60a42a97062f47873c1db61
asloob ahamed <asloobahmd@gmail.com>
2024-12-01 21:06:16 +0530

    added pictures for customer group

--- src/components/CustomerGroup.jsx
+++ src/components/CustomerGroup.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import Person1 from "../assets/images/p1.jpg";
+import Person2 from "../assets/images/p2.jpg";
+import Person3 from "../assets/images/p3.jpg";
 
 const CustomerGroup = () => {
   return (
     <div className="flex -space-x-3">
       <img
-        className="w-10 h-10 border-2 border-white rounded-full hover:-translate-y-1 "
-        src="https://pagedone.io/asset/uploads/1704275541.png"
+        className="w-10 h-10 border-2 border-white rounded-full hover:-translate-y-1 object-cover"
+        src={Person2}
         alt="Stacked rounded avatar"
       />
       <img
         className="w-10 h-10 border-2 border-white rounded-full hover:-translate-y-1 object-cover"
-        src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww"
+        src={Person1}
         alt="Stacked rounded avatar"
       />
       <img
         className="w-10 h-10 border-2 border-white rounded-full hover:-translate-y-1 object-cover"
-        src="https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbSUyMHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D"
+        src={Person3}
         alt="Stacked rounded avatar"
       />
     </div>
