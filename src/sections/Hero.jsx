e1ff56f33dcfd1234e99858e11962b01a4eef1ba
asloob ahamed <asloobahmd@gmail.com>
2024-12-06 15:59:22 +0530

    updated ui

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ import { icons } from "../assets/icons";
 
 const Hero = () => {
   return (
-    <section className="pb-14 md:pb-20 pt-48">
+    <section className="pb-14 md:pb-20 pt-36 smm:pt-40 md:pt-48">
       {/* blurred bg snippet - from this site -> bg.ibelick.com*/}
       <div className="absolute inset-0 -z-10 h-full w-full bg-brand-primary bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
         <div className="absolute left-0 right-0 top-20 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-accent-dark opacity-30 blur-[100px]"></div>
